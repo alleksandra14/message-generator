ed1016d92e3b79bcf9ef7cba83db1e165c549fdd
alleksandra14 <mamajanya14@gmail.com>
2024-11-23 15:57:57 +0000

    split data from script

---
+++ data.js
@@ -0,0 +1,11 @@
+const Wisdom = {
+    signInfo: ['star', 'moon', 'sun', 'comet'],
+    fortuneOutput: ['terrible luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
+    advice: ['go out and eat', 'not read this', 'play more', 'trust no one']
+  }
+
+export default Wisdom
+export const SignInfo = ['star', 'moon', 'sun', 'comet']
+
+// const { signInfo } = Wisdom
+const signInfo = Wisdom.signInfo
--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+import collectiveWisdom from './data.js'
+import { SignInfo } from './data.js'
+
+console.info(SignInfo)
+
 function generateRandomNumber(num) {
     // Gets # from 0 -> num - 1
     return Math.floor(Math.random() * num)
   }
   
-  const collectiveWisdom = {
-    signInfo: ['star', 'moon', 'sun', 'comet'],
-    fortuneOutput: ['terrible luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
-    advice: ['go out and eat', 'not read this', 'play more', 'trust no one']
-  }
+  
   
   // Store the 'wisdom' in an array
   let personalWisdom = []
